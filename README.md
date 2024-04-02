36d88366f477f10131e822b244f1cca874a11efa
larbing <larbing@163.com>
2024-04-02 14:53:57 +0800

    up

--- README.md
+++ README.md
@@ -9,6 +9,12 @@ settings.py 文件下 修改成你本地的目录配置 INDEXDIR = "xxxx"
 
 
 #### 运行爬虫 
+~~~
 cd scrapy-video-spider
 
 scrapy crawl ffzy5
+
+或者
+
+运行 run.py
+~~~
--- main.py
+++ main.py
@@ -1,35 +1,42 @@
 from whoosh.index import create_in , open_dir
 from whoosh.fields import *
 from whoosh.writing import BufferedWriter
-from whoosh.qparser import QueryParser
+from whoosh.qparser import QueryParser,AndGroup
+from whoosh.query import *
 from jieba.analyse import ChineseAnalyzer
 
-import os.path
-import sys
-import subprocess
+# import os.path
+# import sys
+# import subprocess
 
-# analyzer = ChineseAnalyzer()
-# schema = Schema(
-#         id=ID(stored=True),
-#         title=TEXT(stored=True,analyzer=analyzer), 
-#         content=TEXT(analyzer=analyzer),
-#         url=STORED,
-#         region=KEYWORD(analyzer=analyzer),
-#         category=KEYWORD(analyzer=analyzer),
-#         update_context=KEYWORD(analyzer=analyzer)
-# )
-# ix = open_dir("/home/rock/app/indexdir",schema=schema)
+from myspider.settings import INDEXDIR
+
+analyzer = ChineseAnalyzer()
+schema = Schema(
+        id=ID(stored=True),
+        title=TEXT(stored=True,analyzer=analyzer), 
+        content=TEXT(analyzer=analyzer),
+        url=STORED,
+        region=KEYWORD(analyzer=analyzer),
+        category=KEYWORD(analyzer=analyzer),
+        update_context=KEYWORD(analyzer=analyzer)
+)
+ix = open_dir(INDEXDIR,schema=schema)
 
 
 
 
 # # 创建一个查询解析器
-# qp = QueryParser("content", ix.schema)
+qp1 = QueryParser("content", ix.schema)
+qp2 = QueryParser("content", ix.schema)
 
-# # 执行查询
-# with ix.searcher() as searcher:
-#     query = qp.parse("五行世家")
-#     results = searcher.search(query,limit=50)
-#     for result in results:
-#         print(result)
+query1 = qp1.parse("星之金币续集")
+query2 = qp2.parse("与凤行")
+
+
+# 执行查询
+with ix.searcher() as searcher:
+    results = searcher.search(Or([query1,query2]))
+    for result in results:
+        print(result)
 
