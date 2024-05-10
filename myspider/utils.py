f5d6ceae521b8447951049685ed5ed2554525ef0
larbing <larbing@163.com>
2024-05-10 20:06:38 +0800

    up

--- myspider/utils.py
+++ myspider/utils.py
@@ -6,15 +6,6 @@ from hashlib import md5
 
 from myspider.settings import IDDIR
 
-"""
-    Calculate the MD5 hash of the input string.
-
-    Parameters:
-    string (str): The input string to calculate the MD5 hash for.
-
-    Returns:
-    str: The MD5 hash of the input string.
-"""
 def md5_hash(string:str):
     return md5(string.encode('utf-8')).hexdigest()
 
