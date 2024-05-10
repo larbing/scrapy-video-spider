d67ddb325c8e5dee211293939f20d7cbaee2d4fb
larbing <larbing@163.com>
2024-05-10 20:14:43 +0800

    up

--- myspider/utils.py
+++ myspider/utils.py
@@ -14,32 +14,29 @@ class IncrementalIdGenerator:
         self.file_path = file_path
         self.lock = threading.Lock()
         self.id_max   = max
-        self.id_count = max
         self.id_index = int(time.time())
         self.id_step  = 1
-
-        with self.lock:
-            self.preallocate_ids()
+        self.preallocate_ids()
 
     def preallocate_ids(self):
-        if os.path.exists(self.file_path):
-            with open(self.file_path, 'r') as file:
-                self.id_index = int(file.read())
+        with self.lock:
+            if os.path.exists(self.file_path):
+                with open(self.file_path, 'r') as file:
+                    self.id_index = int(file.read())
+            
+            with open(self.file_path, 'w') as file:
+                file.write(str(self.id_index + self.id_max))
         
-        with open(self.file_path, 'w') as file:
-            file.write(str(self.id_index + self.id_max))
-        
-        self.producer = self.infinite_producer()
+            self.producer = self.infinite_producer()
 
     def infinite_producer(self):
         for id in itertools.count(self.id_index,self.id_step):
             yield id
     
     def get_id(self):
-        if self.id_count < 1:
+        new_id = next(self.producer)
+        if new_id >= self.id_index + self.id_max:
             self.preallocate_ids()
-
-        self.id_count -=1
-        return next(self.producer)
+        return new_id
         
 idGenerator = IncrementalIdGenerator()
